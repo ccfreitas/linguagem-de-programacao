b20a3b1676fd7c868ec8c138cbcbdd2d9993f1d7
ccfreitas <88351594+ccfreitas@users.noreply.github.com>
2022-08-21 09:08:59 -0300

    salario

--- exercício-06/salario-funcionário/salario.js
+++ exercício-06/salario-funcionário/salario.js
@@ -3,9 +3,9 @@ function calcular()
     let vendas = document.getElementById("vlr").value
     let salario = document.getElementById("sal").value
 
-    let comi = (vendas * (10/100))
+    let comi = (parseInt(vendas) * (10/100))
     
-    let salf = (salario + comi)
+    let salf = (parseInt(salario) + comi)
 
     document.getElementById("comissão").innerHTML = `Valor da comissão é = $${comi.toFixed(2)}`
     document.getElementById("salariof").innerHTML = `Valor total do salário é = $${salf.toFixed(2)}`
