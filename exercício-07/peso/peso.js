602413f590a4a2102a5dfc6c7d9eedcf49ca0ce9
ccfreitas <88351594+ccfreitas@users.noreply.github.com>
2022-08-21 09:11:35 -0300

    peso

--- exercício-07/peso/peso.js
+++ exercício-07/peso/peso.js
@@ -5,6 +5,6 @@ function calcular()
     let fat = (parseInt(peso) + (peso * (15/100)))
     let slim = (parseInt(peso) - (peso * (20/100)))
 
-    document.getElementById("engordar").innerHTML = `O peso pesado é = ${fat}`
-    document.getElementById("emagrecer").innerHTML = `O peso pena é = ${slim}`
+    document.getElementById("engordar").innerHTML = `O peso pesado é = ${fat} Kg`
+    document.getElementById("emagrecer").innerHTML = `O peso pena é = ${slim} Kg`
 }
