c323fd32732835819f874a056edb2af0d7c897b1
ccfreitas <88351594+ccfreitas@users.noreply.github.com>
2022-08-30 08:38:38 -0300

    operações

--- estruturas de seleção/exercícios/operações/operações.js
+++ estruturas de seleção/exercícios/operações/operações.js
@@ -2,14 +2,16 @@ function calcular()
 {
     let n1 = Number(document.getElementById("n1").value)
     let n2 = Number(document.getElementById("n2").value)
+    let escolha = document.getElementById("escolha").value
+
+    let erro = false
     let valor = 0
 
-    let escolha
-    if (escolha = "média")
+    if (escolha == "média")
         {
             valor = (n1 + n2) / 2
         }
-    else if (escolha = "subtração")
+    else if (escolha == "subtração")
         {
             if (n1 > n2)
             {
@@ -20,16 +22,34 @@ function calcular()
                 valor = (n2 - n1)
             }
         }
-    else if (escolha = "multiplicação")
+    else if (escolha == "multiplicação")
         {
             valor = (n1 * n2)
         }
-    else if (escolha = "divisão")
+    else if (escolha == "divisão")
         {
             if (n2 != 0)
-            valor = (n1 / n2)
+            {  
+                 valor = (n1 / n2)
+            }
+            else
+            {
+                erro = true
+            }
         }
+    else
+    {
+        erro = true
+    }
+    if (erro)
+    {
+        document.getElementById("operador").innerHTML = `Erro no programa, tente novamente`
+    }
+    else
+    {
+        document.getElementById("operador").innerHTML = `A ${escolha} é = ${valor}` 
+    }
      
-    document.getElementById("operador").innerHTML = `A ${escolha} é = ${valor}`
+    
 
 }
