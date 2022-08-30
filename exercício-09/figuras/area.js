acffc021f74533c44c8bcd6d65fb76a475e207c9
ccfreitas <88351594+ccfreitas@users.noreply.github.com>
2022-08-30 09:11:19 -0300

    area

--- exercício-09/figuras/area.js
+++ exercício-09/figuras/area.js
@@ -1,10 +1,10 @@
 function calcular()
 {
-    let Bma = document.getElementById("bma").value
-    let Bme = document.getElementById("bme").value
-    let Lpa = document.getElementById("lp").value
+    let Bma = Number(document.getElementById("bma").value)
+    let Bme = Number(document.getElementById("bme").value)
+    let Lpa = Number(document.getElementById("lp").value)
 
-    let area = ((Bma+ Bme) * (Lpa))/2
+    let area = ((Bma+ Bme) * Lpa) / 2
 
     document.getElementById("resultado").innerHTML = `A área do trapézio é = ${area}`
 }
